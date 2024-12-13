76045c662c3a44181cda80a4a2af28c55b45225b
Chris <chriskwanmy@gmail.com>
2024-12-13 13:59:49 -0700

    deleted comments

--- update.py
+++ update.py
@@ -7,10 +7,8 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 import gspread
 
-# 设置 ChromeDriver 路径
-chromedriver_path = "chromedriver.exe"  # 替换为你的 ChromeDriver 路径
+chromedriver_path = "chromedriver.exe"  
 
-# 启动浏览器
 service = Service(chromedriver_path)
 driver = webdriver.Chrome(service=service)
 
@@ -41,29 +39,14 @@ def click_element_with_retry(driver, by_method, value, retry=200):
     print("Exceeded maximum retries. Couldn't click the element.")
     raise ElementClickInterceptedException(f"Element with {by_method}: {value} could not be clicked after {retry} attempts")
 
-# Google Sheets 初始化
+
 gc = gspread.service_account('service_account.json')
 spreadsheet = gc.open('JHC')
 sheet = spreadsheet.get_worksheet(0)
-# 所有链接
+
 urls = sheet.get_all_values()
 urls = [url[0] for url in urls]
-# urls = [
-#     "https://www.jhceshop.com/hk_tc/6912504332232.html",
-#     "https://www.jhceshop.com/hk_tc/4891203502394.html",
-#     "https://www.jhceshop.com/hk_tc/4891203053902.html",
-#     "https://www.jhceshop.com/hk_tc/4891203499502.html",
-#     "https://www.jhceshop.com/hk_tc/4891203051403.html",
-#     "https://www.jhceshop.com/hk_tc/4891203501748.html",
-#     "https://www.jhceshop.com/hk_tc/6912504338371.html",
-#     "https://www.jhceshop.com/hk_tc/068060463494.html",
-#     "https://www.jhceshop.com/hk_tc/051131830271.html"
-# ]
 
-
-# 存储每个链接的工作表索引
-
-# 遍历每个链接
 for index, url in enumerate(urls):
     if (url == ''):
         print(f"============ Skipping {index} == {url} ============")
@@ -71,19 +54,14 @@ for index, url in enumerate(urls):
     print(f"============ Start updating == {index} == {url} ============")
     driver.get(url)
     
-
-    # 等待页面完全加载
     target = find_element_with_retry(driver,By.XPATH, "//*[contains(text(), '型號')]")
     driver.execute_script("arguments[0].scrollIntoView();", target)
     click_element_with_retry(driver, By.XPATH, "//*[contains(text(), '查看門市庫存')]", 200)
-    time.sleep(3)  # 可根据实际情况调整等待时间
-
-    # 选择区域并提取店铺信息
+    time.sleep(3) 
     regions = ['香港區', '九龍區', '新界區', '離島區/偏遠地區', '澳門區']
     store_data = []
 
     for region in regions:
-        # 选择区域
         select_element = Select(find_element_with_retry(driver, By.ID, 'region-root-W41'))
         print("Got region select element.")
         try:
@@ -93,52 +71,44 @@ for index, url in enumerate(urls):
         except NoSuchElementException:
             print(f"Not found region: {region}")
         
-        # 等待页面更新
-        time.sleep(3)  # 可根据实际情况调整等待时间
+        time.sleep(3) 
         
-        # 获取产品标题
         product_title_element = find_element_with_retry(driver, By.CLASS_NAME, 'productFullDetail-productName-uvJ')
         print("Product title element found." + product_title_element.text)
         print("===================")
-        product_title = product_title_element.text.strip()  # 获取并去除前后空格
+        product_title = product_title_element.text.strip()
 
-        # 提取店铺信息
         address_list = find_element_with_retry(driver, By.CLASS_NAME, 'shoplistRoot', 200)
-        li_elements = address_list.find_elements(By.TAG_NAME, 'li')  # 选择合适的选择器
+        li_elements = address_list.find_elements(By.TAG_NAME, 'li') 
         for li in li_elements:
-            lines = li.text.split('\n')  # 按行分割文本
-            if len(lines) >= 4:  # 确保有足够的行
-                address = lines[0].strip()  # 地址
-                phone = lines[1].strip()     # 电话
-                opening_hours = lines[2].strip()  # 开门时间
-                stock_status = lines[3].strip()    # 存货状态
+            lines = li.text.split('\n') 
+            if len(lines) >= 4:  
+                address = lines[0].strip()  
+                phone = lines[1].strip()     
+                opening_hours = lines[2].strip()  
+                stock_status = lines[3].strip()    
             
                 store_data.append({
                     'Region': region,
-                    'Product Title': product_title,  # 添加产品标题
+                    'Product Title': product_title,
                     'Address': address,
                     'Phone': phone,
                     'Opening Hours': opening_hours,
                     'Stock Status': stock_status
                 })
-    # 转换为 DataFrame
     df = pd.DataFrame(store_data)
     print(f"============{url} Information Done============")
 
-    # 获取相应的工作表    
-    sheet_index = index + 1  # 从第8个工作表开始
+    sheet_index = index + 1  
     sheet = spreadsheet.get_worksheet(sheet_index)
-    sheet.clear()  # 清除现有数据
+    sheet.clear() 
 
-    # 将数据写入 Google Sheets
     values = [df.columns.values.tolist()] + df.values.tolist()
-    sheet.update('A1', values)  # 从单元格 A1 开始写入数据
+    sheet.update('A1', values) 
 
     print(f"============== Updated {len(values)} Rows to Google Sheets Sheet {sheet_index}.")
 
-    # 更新工作表索引
-    sheet_index += 1  # 下一个工作表
+    sheet_index += 1  
 
-# 关闭浏览器
 print("===================")
 driver.quit()
